75f8851889c6ec2fdec086f2694954778a694fbb
drimota <driismota@hotmail.com>
2022-07-26 10:47:56 -0400

    segunda atividade completa

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,8 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const minhaString = "Eu não sou supersticioso, mas sou um pouco __.      ";
+const semEspaco = minhaString.trim()
+
+console.log(minhaString.length)
+console.log(minhaString.trim())
+console.log(semEspaco)
+console.log(semEspaco.length)
+console.log(minhaString.replaceAll("__","sticioso"))
